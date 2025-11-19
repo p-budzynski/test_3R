39ee6a964f66525b1c29b1794b16bc31df2c4e96
p-budzynski <p.budzynski@op.pl>
2025-11-19 21:09:03 +0100

    fixed bugs

--- src/main/java/pl/kurs/mapper/SubscriptionMapper.java
+++ src/main/java/pl/kurs/mapper/SubscriptionMapper.java
@@ -3,9 +3,6 @@ package pl.kurs.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import pl.kurs.dto.SubscriptionDto;
-import pl.kurs.entity.Author;
-import pl.kurs.entity.Category;
-import pl.kurs.entity.Client;
 import pl.kurs.entity.Subscription;
 
 @Mapper(componentModel = "spring")
@@ -16,29 +13,9 @@ public interface SubscriptionMapper {
     @Mapping(target = "categoryId", source = "category.id")
     SubscriptionDto entityToDto(Subscription entity);
 
-    @Mapping(target = "client", expression = "java(clientFromId(dto.getClientId()))")
-    @Mapping(target = "author", expression = "java(authorFromId(dto.getAuthorId()))")
-    @Mapping(target = "category", expression = "java(categoryFromId(dto.getCategoryId()))")
+    @Mapping(target = "client", source = "clientId")
+    @Mapping(target = "author", source = "authorId")
+    @Mapping(target = "category", source = "categoryId")
     Subscription dtoToEntity(SubscriptionDto dto);
 
-    default Client clientFromId(Long id) {
-        if (id == null) return null;
-        Client client = new Client();
-        client.setId(id);
-        return client;
-    }
-
-    default Author authorFromId(Long id) {
-        if (id == null) return null;
-        Author author = new Author();
-        author.setId(id);
-        return author;
-    }
-
-    default Category categoryFromId(Long id) {
-        if (id == null) return null;
-        Category category = new Category();
-        category.setId(id);
-        return category;
-    }
 }
